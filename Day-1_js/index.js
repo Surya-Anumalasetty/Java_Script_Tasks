d8193e779f503148834e4e91979a247faa450ac1
Surya-Anumalasetty <20kq1a0541cse@gmail.com>
2025-01-27 11:53:50 +0530

    First Task

---
+++ Day-1_js/index.js
@@ -0,0 +1,11 @@
+// Using var
+var greeting = "Hello, world!";
+console.log("Var:", greeting);
+
+// Using let
+let age = 25;
+console.log("Let:", age);
+
+// Using const
+const pi = 3.14159;
+console.log("Const:", pi);
